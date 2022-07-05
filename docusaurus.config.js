e5a07347edd496e25cab18ac3ee44a2b1361b0f7
MasterPtato <maxpopkov.contact@gmail.com>
2022-07-05 16:19:21 -0700

    Fix routing

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -33,14 +33,13 @@ const config = {
 			/** @type {import('@docusaurus/preset-classic').Options} */
 			({
 				docs: {
-					routeBasePath: '/',
 					sidebarPath: require.resolve('./sidebars.js'),
 					// Please change this to your repo.
 					// Remove this to remove the "edit this page" links.
 					editUrl:
 						'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
 					docLayoutComponent: '@theme/DocPage',
-					docItemComponent: '@theme/ApiItem' // Derived from docusaurus-theme-openapi-docs
+					docItemComponent: '@theme/ApiItem'
 				},
 				blog: {
 					showReadingTime: true,
