a6b24e190d440874421544af8ea05595a46e7113
Nathan Flurry <developer@nathanflurry.com>
2022-07-23 19:54:19 +0000

    Fix error

--- src/components/LoomEmbed/index.tsx
+++ src/components/LoomEmbed/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function LoomEmbed({src}): JSX.Element {
 	return (
-		<div style={{position: 'relative', 'padding-bottom': '62.5%', 'height': 0}}>
-			<iframe src={src} frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen style={{position: 'absolute', 'top': 0, left: 0, width: '100%', height: '100%'}}></iframe>
+		<div style={{position: 'relative', 'paddingBottom': '62.5%', 'height': 0}}>
+			<iframe src={src} frameborder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen="true" style={{position: 'absolute', 'top': 0, left: 0, width: '100%', height: '100%'}}></iframe>
 		</div>
 	);
 }
