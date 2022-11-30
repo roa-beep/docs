7d713657dc1d4017e1dde6b88d6cc3b5781061bd
Nathan Flurry <developer@nathanflurry.com>
2022-11-30 08:54:14 +0000

    Add demo

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -74,6 +74,6 @@ export default function HomepageOverview(): JSX.Element {
 	// 	</section>
 	// );
 	return (<div>
-			<iframe width="560" height="315" src="https://www.youtube.com/embed/OVicp0hKTEs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			<iframe style={{display: 'block', margin: '50px auto'}} width="600" height="400" src="https://www.youtube.com/embed/OVicp0hKTEs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 	</div>);
 }
